726ff6f452ed6fde949e648be824bac45fb47490
Jarne-Theys <jarne.theys@gmail.com>
2024-10-26 17:44:22 +0200

    art and dogs

--- Assets/Scripts/DogMovement.cs
+++ Assets/Scripts/DogMovement.cs
@@ -10,6 +10,8 @@ public class DogMovement : MonoBehaviour
     private GameObject dog;
     private GameObject npc;
 
+    private Vector3 targetPosition;
+
     void Start()
     {
         dog = transform.GetChild(0).gameObject;
@@ -19,13 +21,22 @@ public class DogMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float randomTargetX = Random.Range(npc.transform.position.x - wanderRange, npc.transform.position.x + wanderRange);
-        float randomTargetY = Random.Range(npc.transform.position.y - wanderRange, npc.transform.position.y + wanderRange);
-        Vector3 randomTargetPosition = new Vector3(randomTargetX, randomTargetY, 0);
+        if(targetPosition == null || targetPosition == Vector3.zero)
+        {
+            float randomTargetX = Random.Range(npc.transform.position.x - wanderRange, npc.transform.position.x + wanderRange);
+            float randomTargetY = Random.Range(npc.transform.position.y - wanderRange, npc.transform.position.y + wanderRange);
+            targetPosition = new Vector3(randomTargetX, randomTargetY, 0);
+        }
 
         Vector3 currentPosition = dog.transform.position;
 
-        dog.transform.position = Vector3.MoveTowards(currentPosition, randomTargetPosition, 0.1f);
+        dog.transform.position = Vector3.MoveTowards(currentPosition, targetPosition, 0.13f);
+        dog.transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition - currentPosition);
+
+        if (Vector3.Distance(currentPosition, targetPosition) < 0.1f)
+        {
+            targetPosition = Vector3.zero;
+        }
     }
 
 
