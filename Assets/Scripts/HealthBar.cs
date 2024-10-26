50c2bbb98fed5d22cc28cfad16524266a14bc2d3
ViktorLibotteUCLL <113979597+ViktorLibotteUCLL@users.noreply.github.com>
2024-10-26 22:23:59 +0200

    rage bar mostly done

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,15 +1,28 @@
+using Unity.Profiling;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public static HealthBar instance;
     public Slider healthSlider;
     public float maxHealth = 100f;
     public float health;
+    private float timeToDrain = 0.1f;
+    private float healthDrainTimer = -3f;
+    private GameObject player;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
         health = maxHealth;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
@@ -18,8 +31,31 @@ public class HealthBar : MonoBehaviour
         {
             healthSlider.value = health;
         }
+        healthDrainTimer += Time.deltaTime;
+
+        if (healthDrainTimer >= timeToDrain)
+        {
+            takeDamage(25);
+            healthDrainTimer = 0f;
+        }
+
+        if (player == null)
+        {
+            health = 0;
+        }
+
+        if (health == 0)
+        {
+            LevelManager.manager.GameOver();
+            Destroy(player);
+            PlayerMovement.instance.playerAlive = false;
+        }
     }
 
+    public void restoreHealth(float restored)
+    {
+        health += restored;
+    }
     public void takeDamage(float damage)
     {
         health -= damage;   
